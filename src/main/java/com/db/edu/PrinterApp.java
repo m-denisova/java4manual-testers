7c1dd7790816cbff7ac96f4bf14356e3e3f3b52c
PavelEvdokimov <evdokimovpascha@gmail.com>
2016-11-14 10:26:52 +0300

    test commit

--- src/main/java/com/db/edu/PrinterApp.java
+++ src/main/java/com/db/edu/PrinterApp.java
@@ -9,5 +9,6 @@ public class PrinterApp {
     public static void main(String... args) {
         logger.debug("Entering main method with args {}", args);
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
     }
 }
