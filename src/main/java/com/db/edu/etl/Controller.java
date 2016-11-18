3a6cdfcbd2b7b069e5eb141e7db873694f884768
MariaDenisova <mdenisova456@gmail.com>
2016-11-18 11:44:38 +0300

    test commit

--- src/main/java/com/db/edu/etl/Controller.java
+++ src/main/java/com/db/edu/etl/Controller.java
@@ -1,6 +1,13 @@
 package com.db.edu.etl;
+import com.db.edu.EtlApp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class Controller {
+
+    private static final Logger Logger = LoggerFactory.getLogger(Controller.class);
+
     public static String[] transform(RecordType recordType, String rawDataRecord) {
         switch (recordType) {
             case EIS1_DATA_FILE: {
@@ -8,9 +15,24 @@ public class Controller {
             }
             case EIS2_DATA_FILE: {
                 return rawDataRecord.split(";");
-                
             }
-            default: return null;
+            default:
+                return null;
         }
     }
+
+    public static String extract(RecordType recordType) {
+        Logger.debug("Start extract");
+
+        Logger.debug("End extract");
+
+        return null;
+     }
+
+    public static void load (String[] transformedData) {
+        Logger.debug("Start load with ars", transformedData);
+
+        Logger.debug("End load");
+    }
+
 }
